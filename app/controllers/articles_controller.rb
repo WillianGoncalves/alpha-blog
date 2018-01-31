cbb8a87eafb877820f85fb8304c7ae1de41ee3d1
Willian <w.goncalves91@gmail.com>
2018-01-31 20:10:10 -0200

    Lesson 61 - Deleting articles and using partials.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -30,11 +34,15 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
-  
-  def show
-    @article = Article.find(params[:id])
-  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :description)
